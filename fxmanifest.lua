3c79f2041d30da6d45c5d46f5b954daff5bf16cb
Razor-creator <78138354+Razor-creator@users.noreply.github.com>
2021-11-01 18:07:35 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,14 @@ game 'gta5'
 description 'QB-Menu'
 version '0.0.1'
 
-client_script 'client/main.lua'
-
-server_script 'server/main.lua'
+client_scripts {
+    'client/main.lua',
+    'config.lua'
+}
+server_scripts {
+    'server/main.lua',
+    'config.lua'
+}
 
 ui_page 'html/index.html'
 
